974e22b1d35176e7d89d13b2d141bdb226a6d755
yogga <arifyoga9@gmail.com>
2022-05-31 11:23:24 +0700

    add some components

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,33 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Logo from '../components/Logo';
+import Nav from '../components/Nav';
+import Button from '../components/Button';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <>
-      <div>
+      <div className="bg-blue-500 ">
         <div className="container mx-auto">
-          <div className="flex bg-blue-500">
-            <div className="w-3/12 uppercase text-2xl font-semibold tracking-widest">
-              Arif
+          <div className="flex items-center">
+            <div className="w-3/12 ">
+              <Logo/>
             </div>
             <div className="w-6/12">
-              <ul className="flex justify-center space-x-10">
-                <li><a className="text-white text-opacity-60 font-semibold">Profile</a></li>
-                <li><a className="text-white text-opacity-60 font-semibold">Skills</a></li>
-                <li><a className="text-white text-opacity-60 font-semibold">Project</a></li>
-                <li><a className="text-white text-opacity-60 font-semibold">Contact</a></li> 
-              </ul>
+              <Nav/>
             </div>
+            <div className="w-3/12 text-right">
+              <Button>Kontak</Button>
+            </div>
+          </div>
+          <div className="text-center">
+            <h1 className="text-3xl text-white font-semibold font-mono">
+              Saya seorang front-end engineer, rpa engineer, dan juga seo engineer
+            </h1>
+            <p className="text-white text-opacity-60 text-lg">
+              Saya juga suka hiking, futsal, dan bermain game.
+            </p>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Source Sans Pro", "ui-sans-serif", "system-ui"],
+        mono: ["IBM Plex Mono", "ui-monospace"],
+      },
+      colors: {
+            yellow: {
+              "50": "#FDFEEC",
+              "100": "#FBFDDD",
+              "200": "#F7FCBB",
+              "300": "#F4FA99",
+              "400": "#F0F977",
+              "500": "#EBF754",
+              "600": "#E5F415",
+              "700": "#B2BE09",
+              "800": "#777F06",
+              "900": "#3B3F03"
+            },
+        }
+    },
   },
   plugins: [],
 }
